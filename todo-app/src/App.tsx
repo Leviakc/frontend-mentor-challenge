59b20e708c3b73fc7bab560683a9bbcb754f064d
Leviakc <gomen98.jg@gmail.com>
2023-07-25 15:40:16 -0600

    added logic of todos

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,7 +1,19 @@
-export const App = () => { 
-return (
-    <h1 className="text-3xl font-bold underline border-sky-50">
-      Hello world!
-    </h1>
-  )
+import { Header } from "./components/Header";
+import { TodoList } from "./components/TodoList";
+import { useTodos } from "./hooks/useTodo";
+
+export const App = () => {
+  const { pendingTodosCount } = useTodos();
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh] bg-theme-light dark:bg-theme-dark bg-contain bg-no-repeat grid justify-items-center items-center">
+        <div className="">
+          <Header />
+          <TodoList />
+          <footer className="">{pendingTodosCount}</footer>
+        </div>
+      </div>
+    </>
+  );
 };
+// bg-[url('./public/images/bg-desktop-light.jpg')] bg-no-repeat bg-cover dark:bg-[url('./public/images/bg-desktop-dark.jpg')]
