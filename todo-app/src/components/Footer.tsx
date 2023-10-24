24b1e02602e75114873297a85af4d0b9934038b5
Leviakc <gomen98.jg@gmail.com>
2023-10-23 19:51:52 -0600

    deleted projects that are not junior level

--- todo-app/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-
-export const Footer = () => {
-  return (
-    <footer 
-        className="hover:cursor-pointer hover:font-medium hover:text-slate-800 dark:hover:text-slate-200">
-			Footer
-		</footer>
-  )
-}
